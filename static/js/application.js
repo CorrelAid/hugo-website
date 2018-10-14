21ec1a7a692d1a56654a549c3d4980fe98af5701
Jan Dix <dix.jan@gmx.de>
2018-10-14 23:17:51 +0200

    donation form

--- static/js/application.js
+++ static/js/application.js
@@ -6,3 +6,42 @@ setInterval(function(){
         $(this).toggle();
     });
 }, 500);
+
+/* =============== DONATION FUNCTIONALITY =============== */
+
+// initialize variables
+var progress = 0;
+var purpose = 0;
+var type = 0;
+
+// get elements
+$donationSection = $('.donation-section');
+$donate = $('.donate-box-inner');
+$purpose = $('.donate-box-inner.purpose');
+$next = $('#next');
+$back = $('#back');
+
+// change style
+$donate.click(function (e) {
+    $donate.removeClass('active');
+    $(this).addClass('active');
+});
+
+// save purpose
+$purpose.click(function (e) {
+    purpose = $(this).data('purpose');
+    console.info(purpose);
+});
+
+// next button
+$next.click(function (e) {
+    if (progress === 0 && purpose === 0) return;
+    if (progress === 1 && type === 0) return;
+    progress++;
+    $donationSection.hide();
+    $('#' + progress).show();
+});
+
+// back button
+
+
