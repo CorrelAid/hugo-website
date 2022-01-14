1f252750cf21163d2192d48288696599cf322a75
Frie <fr1e@pm.me>
2022-01-14 09:59:44 +0100

    german map with german links wuhu

--- R/mapping-lc.R
+++ R/mapping-lc.R
@@ -10,5 +10,5 @@ map <- correlaidx_map(chapters)
 map %>% saveWidget(file=here::here("static", "html", "map_en.html"))
 
 chapters_de <- get_correlaidx_data('de')
-map_de <- correlaidx_map(chapters, 'de')
+map_de <- correlaidx_map(chapters_de, 'de')
 map_de %>% saveWidget(file=here::here("static", "html", "map_de.html"))
